3871392fc8febb0d52cfc29745a2bf7fb0a0d236
Ye Lin Aung <me@yelinaung.com>
2014-12-25 23:29:52 +0630

    extract some info

--- main.go
+++ main.go
@@ -3,21 +3,54 @@ package main
 import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
+	"os"
+	str "strings"
 )
 
 var baseString = "https://play.google.com/store/apps/details?id="
 
 func main() {
 
-	testPackage := "com.google.android.gm"
-	doc, err := goquery.NewDocument(fmt.Sprintf("%s%s", baseString, testPackage))
+	// testPackage := "com.zwenexsys.yoteshin"
+
+	// Using with file
+	f, err := os.Open("ys.html")
+	PanicIf(err)
+	defer f.Close()
+	doc, err := goquery.NewDocumentFromReader(f)
+	// doc, err := goquery.NewDocument(fmt.Sprintf("%s%s", baseString, testPackage))
 	PanicIf(err)
 
-	doc.Find(".main-content").Each(func(i int, s *goquery.Selection) {
-		title := s.Find(".document-title").Text()
-		desc := s.Find(".id-app-orig-desc").Text()
-		fmt.Printf("%s - %s", title, desc)
+	//	doc.Find(".main-content").Each(func(i int, s *goquery.Selection) {
+	//		title := s.Find(".document-title").Text()
+	//		desc := s.Find(".id-app-orig-desc").Text()
+	//		fmt.Printf("%s - %s", title, desc)
+	//
+	//	})
+
+	tmp := make(map[string]string)
+	// fmt.Println(doc.Find(".meta-info").First().Text())
+	doc.Find(".meta-info").Each(func(i int, s *goquery.Selection) {
+		fieldName := str.TrimSpace(s.Find(".title").Text())
+		switch fieldName {
+		case "Updated":
+			tmp["updated"] = s.Find(".content").Text()
+		case "Installs":
+			tmp["installs"] = s.Find(".content").Text()
+		case "Size":
+			tmp["size"] = s.Find(".content").Text()
+		case "Current Version":
+			tmp["currentVersion"] = s.Find(".content").Text()
+		case "Requires Android":
+			tmp["requiresAndroid"] = s.Find(".content").Text()
+		case "Content Rating":
+			tmp["contentRating"] = s.Find(".content").Text()
+		}
 	})
+
+	for x, y := range tmp {
+		fmt.Printf("%s - %s\n", x, str.TrimSpace(y))
+	}
 }
 
 func PanicIf(err error) {
